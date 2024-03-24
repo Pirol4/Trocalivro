e1219d91ca66a9dea3664b8f27c4954610d3d5d7
Sacramento-20 <sacramento.15@hotmail.com>
2024-03-24 14:35:38 -0300

    Adicionando comentários e o requirements.txt

--- trocalivro/users/admin.py
+++ trocalivro/users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from users.models import User, Book, Address
 
 
-# Register your models here.
+# Registrando os modelos.
 admin.site.register(User)
 admin.site.register(Book)
 admin.site.register(Address)
--- trocalivro/users/models.py
+++ trocalivro/users/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from enum import Enum
 
 
+# Classe Enum para verificar o status de um livro
 class StatusBook(Enum):
   AVAILABLE = 'Available'
   IN_EXCHANGE = 'In Exchange'
   UNAVAILABLE = 'Unavailable'
 
+# Tabela do usuário
 class User(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
@@ -18,6 +20,7 @@ class User(models.Model):
     return f'user = {self.firstname}, email = {self.email}, reputation = {self.reputation}'
 
 
+# Tabela livro
 class Book(models.Model):
   owner = models.ForeignKey(User, on_delete=models.CASCADE)
   status = models.CharField(max_length=20, choices = [(tag.name, tag.value) for tag in StatusBook])
@@ -29,6 +32,7 @@ class Book(models.Model):
     return f'book = {self.name}, description = {self.description}, status = {self.status}, owner = {self.owner.firstname}'
 
 
+# Tabela dos endereços 
 class Address(models.Model):
   owner = models.ForeignKey(User, on_delete=models.CASCADE)
   street = models.CharField(max_length = 255)

b65ec37b3c465152d6648c1fe3a1a240a6488fa4
Sacramento-20 <sacramento.15@hotmail.com>
2024-03-27 17:02:47 -0300

    Criada a relação de um para um do usuario e endereço e um para muito entre usuario e livros no banco de dados.

--- trocalivro/users/models.py
+++ trocalivro/users/models.py
@@ -8,33 +8,23 @@ class StatusBook(Enum):
   IN_EXCHANGE = 'In Exchange'
   UNAVAILABLE = 'Unavailable'
 
+
 # Tabela do usuário
 class User(models.Model):
-  firstname = models.CharField(max_length=255)
-  lastname = models.CharField(max_length=255)
-  email = models.EmailField(default="", null=True)
-  phone_number = models.CharField(max_length=255,default="")
+  firstname = models.CharField(max_length = 255)
+  lastname = models.CharField(max_length = 255)
+  email = models.EmailField(default = "", null = True)
+  phone_number = models.CharField(max_length = 255,default = "")
   reputation = models.IntegerField(default = 5) 
 
   def __str__(self):
     return f'user = {self.firstname}, email = {self.email}, reputation = {self.reputation}'
 
 
-# Tabela livro
-class Book(models.Model):
-  owner = models.ForeignKey(User, on_delete=models.CASCADE)
-  status = models.CharField(max_length=20, choices = [(tag.name, tag.value) for tag in StatusBook])
-  name = models.CharField(max_length=255)
-  description = models.TextField()
-  # image = models.ImageField(upload_to='books/', blank=True, null=True)
-
-  def __str__(self):
-    return f'book = {self.name}, description = {self.description}, status = {self.status}, owner = {self.owner.firstname}'
-
-
 # Tabela dos endereços 
 class Address(models.Model):
-  owner = models.ForeignKey(User, on_delete=models.CASCADE)
+  owner_address = models.OneToOneField(User, on_delete=models.CASCADE, primary_key = True)
+  # owner = models.ForeignKey(User, on_delete=models.CASCADE)
   street = models.CharField(max_length = 255)
   number = models.CharField(max_length = 255)
   complement = models.CharField(max_length = 255)
@@ -43,8 +33,24 @@ class Address(models.Model):
   zip_code = models.CharField(max_length = 255)
 
   def __str__(self):
-    return f'user = {self.owner.firstname}, street = {self.street}, complement = {self.complement}, city = {self.city}, country = {self.country}'
+    return f'street = {self.street}, complement = {self.complement}, city = {self.city}, country = {self.country}'
     
+
+
+# Tabela livro
+class Book(models.Model):
+  owner = models.ForeignKey(User, on_delete = models.CASCADE)
+  status = models.CharField(max_length = 20, choices = [(tag.name, tag.value) for tag in StatusBook])
+  name = models.CharField(max_length = 255)
+  description = models.TextField()
+  # image = models.ImageField(upload_to='books/', blank=True, null=True)
+
+  def __str__(self):
+    return f'book = {self.name}, description = {self.description}, status = {self.status}, owner = {self.owner.firstname}'
+
+
+
+
 
 
 

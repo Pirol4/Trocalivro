46a8c454c74771462ba7d5c95d61e691d1494b41
estersassis <estersarasilva@gmail.com>
2024-04-17 19:05:16 -0300

    feat(initial) pages

--- trocalivro/library/models.py
+++ trocalivro/library/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse 
 from enum import Enum
 from datetime import datetime
 import uuid
@@ -19,12 +20,14 @@ class User(models.Model):
   address = models.TextField(default = "")
 
 class Book(models.Model):
-  id = models.UUIDField(primary_key=True, default=uuid.uuid4)
   title = models.CharField(max_length = 255)
   description = models.TextField()
   genre = models.CharField(max_length=200, default = "", null = True)
-  image = models.ImageField(upload_to='books-mages/', blank=True, null=True)
+  image = models.ImageField(upload_to='library/static/images/', blank=True, null=True)
   status = models.CharField(max_length = 20, choices = [(tag.name, tag.value) for tag in StatusBook])
   
   created_at = models.DateField(default=timezone.now)
   owner = models.ForeignKey(User, on_delete = models.CASCADE)
+
+  def get_absolute_url(self):
+    return reverse('book-detail', args=[str(self.id)])
--- trocalivro/library/urls.py
+++ trocalivro/library/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from library import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('book/<int:id>', views.book_detail_view, name='book-detail')
 ]

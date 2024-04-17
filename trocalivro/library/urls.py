7ab75a69cf8d87045d9f2b2412bab17539586622
estersassis <estersassis@BOOK-L0OEQCNTSO>
2024-04-17 20:25:17 -0300

    feat(auth) add auth

--- trocalivro/library/urls.py
+++ trocalivro/library/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from library import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -6,5 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('book/<int:id>', views.book_detail_view, name='book-detail')
+    path('book/<int:id>', views.book_detail_view, name='book-detail'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('profile/', views.profile, name='users-profile')
 ]

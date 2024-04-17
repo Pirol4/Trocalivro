4bbd7c4f41902b2fef1ab4e617e7604faee158d5
estersassis <estersassis@BOOK-L0OEQCNTSO>
2024-04-17 19:05:16 -0300

    feat(initial) pages

--- trocalivro/library/views.py
+++ trocalivro/library/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from django.views import generic
+from django.shortcuts import get_object_or_404
 from library.models import Book, User
 
 
 def index(request):
     num_books = Book.objects.all().count()
+    book_list = Book.objects.all()
 
     context = {
-        'num_books': num_books
+        'num_books': num_books,
+        'book_list': book_list
     }
 
     return render(request, 'index.html', context=context)
+
+def book_detail_view(request, id):
+    book = Book.objects.get(id=id)
+    
+    return render(request, 'book_detail.html', context={'book': book})
